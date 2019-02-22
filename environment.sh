82fd6717fb2ed7178936f3c3c0a737222b06e5c8
Adam Thornton <ademus4@gmail.com>
2019-02-22 14:12:39 -0500

    Removed bloat from python and jupyter from original

---
+++ environment.sh
@@ -0,0 +1,9 @@
+source /usr/local/bin/thisroot.sh
+export DISPLAY=localhost:0.0
+export ROOTSYS=/usr/local/bin/root
+
+# set clas12tool environment
+export CLAS12TOOL=/work/Clas12Tool/
+
+# aliases
+alias ll="ls -l"
